3329aca504d8b5a471a02d2f82c51e98260d885c
João Lucas <joaolucascp16@gmail.com>
2022-07-18 10:30:46 -0300

    app.js: working well with ejs template

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express'),
     app = express(),
+    path = require('path'),
     { readdirSync } = require('fs'),
     dotenv = require('dotenv'),
     mongoose = require('mongoose')
 
 dotenv.config()
 app.use(express.json())
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, '/public'))
+app.set('view engine', 'ejs')
 
-
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
+// getting routes files .js
+readdirSync('./src/routes').map(r => {
+    app.use('/', require('./src/routes/' + r))
 })
-.then(() => console.log(' |> The database has been connected'))
-.catch(() => { console.log(' |> There was an error trying to connect to the database')})
 
+// try to connect to the database
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+    .then(() => console.log(' |> The database has been connected'))
+    .catch(() => console.log(' |> There was an error trying to connect to the database'))
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log('|> The app is running on port: ' + PORT)
-})
+app.listen(PORT, () => console.log(' |> The app is running on port: ' + PORT))
